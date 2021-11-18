8f41c6509a6adb207d47dbe292b5f818e9ea84aa
jarnovi <j.vihervaara@gmail.com>
2021-11-18 18:31:42 +0200

    uusimman excel-tiedoston lataamiseen

--- src/fetcher.php
+++ src/fetcher.php
@@ -1,5 +1,17 @@
 <?php
 /** Fetching the excel sheet from Alko. */
-
+function fetchXlxs(){
+    // Initialize a file URL to the variable
+    $url = 'https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx';
+    
+    $file_name = basename($url);
+    
+    if(file_put_contents( $file_name,file_get_contents($url))) {
+        echo "File downloaded successfully";
+    }
+    else {
+        echo "File downloading failed.";
+    }
+}
 
 ?>
