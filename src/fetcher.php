25b54312c04ff64639ad2cf27c5095abf4ecc805
jarnovi <j.vihervaara@gmail.com>
2021-11-18 18:34:08 +0200

    uusimman excel-tiedoston lataamiseen -typos

--- src/fetcher.php
+++ src/fetcher.php
@@ -1,6 +1,6 @@
 <?php
 /** Fetching the excel sheet from Alko. */
-function fetchXlxs(){
+function fetchXlsx(){
     // Initialize a file URL to the variable
     $url = 'https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx';
     
