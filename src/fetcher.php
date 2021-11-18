9a0b9b4e90fd0f8f3ed83003f0300cf5d56b6474
ljoonal <ljoonal@ljoonal.xyz>
2021-11-18 21:05:18 +0200

    Make fetcher just return the contents without fs

--- src/fetcher.php
+++ src/fetcher.php
@@ -1,17 +1,8 @@
 <?php
 /** Fetching the excel sheet from Alko. */
-function fetchXlxs(){
+function fetch_xlxs() {
     // Initialize a file URL to the variable
     $url = 'https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx';
-    
-    $file_name = basename($url);
-    
-    if(file_put_contents( $file_name,file_get_contents($url))) {
-        echo "File downloaded successfully";
-    }
-    else {
-        echo "File downloading failed.";
-    }
+    return file_get_contents($url);
 }
-
 ?>
