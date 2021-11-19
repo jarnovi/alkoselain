995a2a6f5ccbcb095a20a63df0a010efbb6e6c60
ljoonal <ljoonal@ljoonal.xyz>
2021-11-19 04:47:26 +0200

    Set updated data as completed

--- src/refresh.php
+++ src/refresh.php
@@ -17,6 +17,7 @@ assert($import_batch_id  != null);
 $importer->set_import_batch_id($import_batch_id);
 assert(sizeof($importer->drinks) > 0);
 $db->add_drinks($importer->drinks);
+$db->set_import_batch_as_completed($import_batch_id);
 ?>
 
 
@@ -28,11 +29,13 @@ $db->add_drinks($importer->drinks);
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>AlkoSelain</title>
 	<link href="./style.css" rel="stylesheet" />
+	<meta http-equiv="refresh" content="10; url=index.php">
 </head>
 <body>
 	<main>
 		<h1>Ok</h1>
 		<p>Data päivitetty onnistuneesti.</p>
+		<p>Siirry <a href="index.php">takaisin.</a></p>
 	</main>
 </body>
 </html>
