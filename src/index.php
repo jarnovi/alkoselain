5a9265110b24fb4028f752f9af64fca4303f1027
ljoonal <ljoonal@ljoonal.xyz>
2021-12-11 15:16:40 +0200

    Implement working filtering

--- src/index.php
+++ src/index.php
@@ -4,16 +4,17 @@ $db = require "db.php";
 $table_creator = require "table.php";
 require_once "filter_query_generator.php";
 
-$COLUMNS=["number","name","manufacturer","size","price","price_per_liter","type","origin","vintage","percentage","energy"];
+$COLUMNS = ["number", "name", "manufacturer", "size", "price", "price_per_liter", "type", "origin", "vintage", "percentage", "energy"];
 
+$table_creator = new TableCreator($COLUMNS);
 
 $page = max(0, (int)($_GET['sivu'] ?? 0));
 $amount = min(100, max(1, (int)($_GET['maara'] ?? 25)));
 
-$min_price = $_GET["min-hinta"] ?? null;
-$max_price = $_GET["max-hinta"] ?? null;
-$min_size = $_GET['min-koko'] ?? null;
-$max_size = $_GET["max-koko"] ?? null;
+$min_price = $_GET["min-hinta"] ? (int)((float)$_GET["min-hinta"] * 100) : null;
+$max_price = $_GET["max-hinta"] ? (int)((float)$_GET["max-hinta"] * 100) : null;
+$min_size = $_GET['min-koko'] ? (int)((float)$_GET["min-koko"] * 1000) : null;
+$max_size = $_GET["max-koko"] ? (int)((float)$_GET["max-koko"] * 1000) : null;
 $type = $_GET["tyyppi"] ?? null;
 $origin = $_GET["maa"] ?? null;
 
@@ -22,28 +23,57 @@ $latest_import_batch = $db->get_latest_import_batch();
 
 $filter_query_generator = new FilterQueryGenerator($latest_import_batch->id);
 $filter_query_generator->amount = $amount;
-$filter_query_generator->start = $page*$amount;
+$filter_query_generator->start = $page * $amount;
+
+if ($min_price != null) {
+    $table_creator->min_price = $min_price;
+    $filter_query_generator->filter_by_min_price($min_price);
+}
+if ($max_price != null) {
+    $table_creator->max_price = $max_price;
+    $filter_query_generator->filter_by_max_price($max_price);
+}
+
+if ($min_size != null) {
+    $table_creator->min_size = $min_size;
+    $filter_query_generator->filter_by_min_price($min_size);
+}
+if ($max_size != null) {
+    $table_creator->max_size = $max_size;
+    $filter_query_generator->filter_by_max_price($max_size);
+}
+if ($type != null) {
+    $table_creator->type = $type;
+    $filter_query_generator->filter_by_type($type);
+}
+if ($origin != null) {
+    $table_creator->origin = $origin;
+    $filter_query_generator->filter_by_country($origin);
+}
 
 $drinks = [];
 
 if ($latest_import_batch) {
-	$drinks = $db->fetch_drinks($filter_query_generator);
+    $drinks = $db->fetch_drinks($filter_query_generator);
 }
-$table_html = $table_creator->create($COLUMNS, $drinks);
+
+$table_html = $table_creator->create($drinks);
 ?>
 <!DOCTYPE html>
-<html lang="fi">
+<html lang="fi ">
+
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>AlkoSelain/</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AlkoSelain/</title>
 </head>
-    <?php
-        echo "<style>" . file_get_contents("./style.css") . "</style>";
-    ?>
+<?php
+echo "<style>" . file_get_contents("./style.css") . "</style> ";
+?>
+
 <body>
-	<header>
+    <header>
         <div class="header-container">
             <h1>Alkon tuotekatalogi</h1>
             <!-- Maybe clean this up, or start using templates? -->
@@ -52,32 +82,34 @@ $table_html = $table_creator->create($COLUMNS, $drinks);
                 echo "<!--" . $latest_import_batch->id . "-->";
                 echo "<div class='info-container'>
                         <p>:Päivitetty viimeksi: </p><br>
-                           <time datetime='".$latest_import_batch->date->format("Y-m-d")."'>".
-                    $latest_import_batch->date->format("d.m.Y")."
+                           <time datetime='" . $latest_import_batch->date->format("Y-m-d") . "'>" .
+                    $latest_import_batch->date->format("d.m.Y") . "
                            </time>
-                      </div>";
-            }   else {
+                      </div> ";
+            } else {
                 echo " <div class='warning-container'>
                        <p>Varoitus! Tietoja ei löytynyt.</p>
                        <p>Hae <br> <a href='./refresh.php' style='color:red;'>uudet tiedot</a></p>
                        <p> Saattaa ladata hetken, ole hyvä ja odota rauhassa :)</p>
-                       </div>";
+                       </div> ";
             }
             ?>
         </div>
-	</header>
+    </header>
 
-	<main>
+    <main>
         <form class='main-container' method="GET">
             <div class='table-container'>
-                <?php echo $table_html; ?>
+                <?= $table_html; ?>
             </div>
             <div class='buttons-container'>
                 <input type="hidden" name="maara" value="25">
-                <button type="submit" name="sivu" value="<?= max(0, $page -1) ?>">Edellinen sivu</button>
-                <button type="submit" name="sivu" value="<?= $page +1 ?>">Seuraava sivu</button>
+                <button type="submit" name="sivu" value="<?= max(0, $page - 1) ?>">Edellinen sivu</button>
+                <button type="submit" name="sivu" value="<?= max(0, $page) ?>">Hae nykyinen sivu uudelleen</button>
+                <button type="submit" name="sivu" value="<?= $page + 1 ?>">Seuraava sivu</button>
             </div>
         </form>
-	</main>
+    </main>
 </body>
+
 </html>
--- src/table.php
+++ src/table.php
@@ -19,9 +19,20 @@ class TableCreator {
 		"percentage" => "alkoholi %",
 		"energy" => "energia"
 	];
-	
-	public function create(array $columns_to_display, array $drinks): string {
-		
+
+	private array $columns_to_display;
+
+	public null|int $min_size = null;
+	public null|int $max_size = null;
+	public null|int $min_price = null;
+	public null|int $max_price = null;
+	public null|string $type = null;
+	public null|string $origin = null;
+	public null|int $min_energy = null;
+	public null|int $max_energy = null;
+
+	public function __construct(array $columns_to_display)
+	{
 		// Sanity checks, make sure there are names for columns to display.
 		if (count($columns_to_display) <= 0)
 		throw new Exception("Error in configured columns to display: Need at least 1 column");
@@ -30,22 +41,28 @@ class TableCreator {
 			if (!array_key_exists($column_to_display, TableCreator::COLUMN_NAMES))
 			throw new Exception("Error in configured columns to display: Unknown column");
 		}
-		
+
+		$this->columns_to_display = $columns_to_display;
+	}
+
+	public function create(array $drinks): string
+	{
 		$val = "<table><thead>";
-		$val .= $this->create_header_row($columns_to_display);
-		$val .= $this->create_filter_row($columns_to_display);
+		$val .= $this->create_header_row();
+		$val .= $this->create_filter_row();
 		
 		$val .= "</thead><tbody>";
-		$val .= $this->create_body_rows($columns_to_display, $drinks);
+		$val .= $this->create_body_rows($drinks);
 		$val .= "</tbody></table>";
 		
 		return $val;
 	}
-	
-	private function create_header_row(array $columns_to_display): string {
+
+	private function create_header_row(): string
+	{
 		$val = "<tr>";
-		
-		foreach ($columns_to_display as $column_to_display) {
+
+		foreach ($this->columns_to_display as $column_to_display) {
 			$val .= "<th>" . TableCreator::COLUMN_NAMES[$column_to_display] . "</th>";
 		}
 		
@@ -53,47 +70,49 @@ class TableCreator {
 		$val .= "</tr>";
 		return $val;
 	}
-	
-	private function create_filter_row(array $columns_to_display): string {
+
+	private function create_filter_row(): string
+	{
 		$val = "<tr>";
-		
-			foreach ($columns_to_display as $column) {
-				$val .= "<th>";
-				switch ($column) {
-					case "size":
-						$val .= "<input name='min-koko' type='number' min='0' step='0.1' placeholder='0.5L' />";
-						$val .= "<input name='max-koko' type='number' min='0' step='0.1' placeholder='0.8L' />";
-						break;
-					case "price":
-						$val .= "<input name='min-hinta' type='number' min='0' step='0.01' placeholder='0.00€' />";
-						$val .= "<input name='max-hinta' type='number' min='0' step='0.01' placeholder='20.00€' />";
-						break;
-					case "type":
-						$val .= "<input name='tyyppi' type='text' placeholder='Oluet' />";
-						break;
-					case "origin":
-						$val .= "<input name='maa' type='text' placeholder='Suomi' />";
-						break;
-					case "energy":
-						$val .= "<input name='min-energia' type='number' min='0' step='1' placeholder='0' />";
-						$val .= "<input name='max-energia' type='number' min='0' step='1' placeholder='100' />";
-						break;
-				}
-				
-				$val .= "</th>";
+
+		foreach ($this->columns_to_display as $column) {
+			$val .= "<th>";
+			switch ($column) {
+				case "size":
+					$val .= "<input name='min-koko' type='number' min='0' step='0.1' placeholder='0.5L' value='" . $this->min_size / 1000 . "' />";
+					$val .= "<input name='max-koko' type='number' min='0' step='0.1' placeholder='0.8L' value='" . $this->max_size / 1000 . "'  />";
+					break;
+				case "price":
+					$val .= "<input name='min-hinta' type='number' min='0' step='0.01' placeholder='0.00€' value='" . $this->min_price / 100 . "' />";
+					$val .= "<input name='max-hinta' type='number' min='0' step='0.01' placeholder='20.00€' value='" . $this->max_price / 100 . "' />";
+					break;
+				case "type":
+					$val .= "<input name='tyyppi' type='text' placeholder='Oluet' value='$this->type' />";
+					break;
+				case "origin":
+					$val .= "<input name='maa' type='text' placeholder='Suomi' value='$this->origin' />";
+					break;
+				case "energy":
+					$val .= "<input name='min-energia' type='number' min='0' step='1' placeholder='0' value='" . $this->min_energy . "' />";
+					$val .= "<input name='max-energia' type='number' min='0' step='1' placeholder='100' value='" . $this->max_energy . "' />";
+					break;
 			}
-		
-		return $val ."</tr>";
+
+			$val .= "</th>";
+		}
+
+		return $val . "</tr>";
 	}
-	
-	private function create_body_rows(array $columns_to_display, array $drinks): string {
+
+	private function create_body_rows(array $drinks): string
+	{
 		$val = "";
 		foreach ($drinks as $drink) {
 			assert($drink instanceof Drink);
-			
+
 			$val .= "<tr>";
-			
-			foreach ($columns_to_display as $column) {
+
+			foreach ($this->columns_to_display as $column) {
 				$val .= "<th>";
 				switch ($column) {
 					case "number":
@@ -130,16 +149,13 @@ class TableCreator {
 						$val .= $drink->kcal_per_hundred_ml;
 						break;
 				}
-				
+
 				$val .= "</th>";
 			}
-			
+
 			$val .= "</tr>";
 		}
-		
+
 		return $val;
 	}
 }
-
-return new TableCreator();
-?>
